3e81a14a99091e0d3dedd045c4ebb36b93a9144b
rmob <riley.obryan@gmail.com>
2023-03-27 13:54:53 -0700

    added img to About, form to Contact, skills to Resume

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,20 @@
 import React from "react";
 
+const styles = {
+  listItem: {
+    marginLeft: 5
+  },
+  paddingTop: 200
+}
+
 export default function Footer() {
   return (
-    <div>
-        <p>Footer shit</p>
+    <div className="footer">
+       <ul>
+        <li style={styles.listItem}>Github</li>
+        <li style={styles.listItem}>LinkdIn</li>
+        <li style={styles.listItem}>Twitter</li>
+       </ul>
     </div>
   );
 }
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
+const styles = {
+  avatar: {
+    borderRadius: 50,
+  }
+}
+
 export default function About() {
   return (
     <div>
-      <h1>About Me</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      <h1>About me</h1>
+      <div>
+        <img src="racoondog.avif" width='550px' style={styles.avatar} alt="avatar"/>
+      </div>
+      <p className='about-info'>
+        Hi my name is Riley.  I'm a full stack web-developer now! Thanks for looking at my portfolio page designed using REACT.js motherfucker
       </p>
     </div>
   );
